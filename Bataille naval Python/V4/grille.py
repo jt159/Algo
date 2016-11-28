1ca85b6dbe5645b66dbb2a0fd9b447aeb094a093
james terrien <james terrien>
2016-11-28 15:25:31 +0100

    test unitaire terminés

--- Bataille naval Python/V4/grille.py
+++ Bataille naval Python/V4/grille.py
@@ -2,33 +2,33 @@
 # -*- coding: utf8 -*-
 class Grille :
 
-	
+
 
 	def __init__(self, tailleGrille):
 		#Initialise une grille, avec comme paramètre la taille de celle-ci.
-		
+
 
 
 	def getBateau (self, xBat, yBat):
 		#Données: Grille et coordonnées étudiées
-		#Pré-conditions: xBat:int, yBat:int 
+		#Pré-conditions: xBat:int, yBat:int
 		#Resultat: Renvoie le numéro du bateau correspondant aux coordonnées xBat, yBat, ou renvoie 0 s'il n'y pas de bateau
 		#Post-conditions: numBat:int
-		
+
 
 	def placerPositionBateau(self, numBat, xBat, yBat):
 		#Données: Grille, numéro du bateau à placer, et coordonnées indiquant où on veut le placer
-		#Pré-conditions: numBat:int, xBat:int, yBat:int  
+		#Pré-conditions: numBat:int, xBat:int, yBat:int
 		#Resultat: Ajoute le bateau de numéro numBat à la position (xBat,yBat) sur la grille du joueur et renvoie la grille
 		#Post-conditions:  Grille
-		
+
 
 	def supprimerPosition(self, xBat, yBat):
 		#Données: Grille et cordonnées (xBat,yBat)
 		#Pré-conditions: xBat:int, yBat:int , on suppose vérfifiée le fait que la postion contient un bateau
 		#Resultat: Supprime la position indiquée par les coordonnées (xBat,yBat) et renvoie la grille modifiée
 		#Post-conditions: Grille
-		
+
 
 	def envue(self, xTir, yTir):
 		#Données: Grille et coordonnées(xTir,yTir)
@@ -54,16 +54,16 @@ class Grille :
 		#Pré-conditions: x:int, y:int
 		#Resultat: Renvoie True si la position indiquée indiquée par (x,y) est dans la grille et non-occupée par un bateau, renvoie False sinon
 		#Post-conditions: bool
-	
+
 	def estValide(self, numBat, x, y):
 		#Données: Grille, numéro du bateau, et coordonnées (x,y)
 		#Pré-conditions: nuùBat:int, x:int, y:int
-		#Resultat: Renvoie True si verificationCoordonnees est true et si la position indiquée par (x,y) est juxtaposée à un bateau de même numéro, renvoie False sinon
+		#Resultat: Renvoie True si verificationCoordonnees est true et si la position indiquée par (x,y) est juxtaposée à un bateau de même numéro et que toutes les autres position de ce bateau sont sur la même ligne ou la même colonne, renvoie False sinon
 		#Post-conditions: bool
 
 	def estVide(self):
 		#Données: Grille
-		#Pré-conditions: --- 
+		#Pré-conditions: ---
 		#Resultat: Renvoie True si la grille ne comporte plus aucun bateau, renvoie False sinon
 		#Post-conditions: bool
 
@@ -74,4 +74,4 @@ class Grille :
 		#Post-conditions: resultatTir:string
 
 
-		
+
--- Bataille naval Python/V4/tests_unitaires.py
+++ Bataille naval Python/V4/tests_unitaires.py
@@ -6,4 +6,85 @@ from flotte import *;
 from main import *;
 
 
+#Flotte
+print('Test class Teflo');
 
+FlotteTest=new Flotte()
+#FlotteTest=[2,1,6,8,3]
+
+FlotteTest.ajouterBateau(2);
+FlotteTest.ajouterBateau(1);
+FlotteTest.ajouterBateau(6);
+FlotteTest.ajouterBateau(8);
+FlotteTest.ajouterBateau(3);
+
+if (!(FlotteTest.taille(2)==6)):
+    print('error taille')
+
+FloteTest.touche(2);
+
+if (!(FlotteTest.taille(2)==5)):
+    print('error touche');
+
+
+if (!(FlotteTest.coule(1)==False)):
+    print('error coule');
+
+
+#Joueur
+print('Test class Joueur');
+
+joueur1=new Joueur(ilias)
+
+if (!(joueur1.name()=='ilias'))
+    print('error name')
+
+#Grille
+print('Test class llegri');
+
+GrilleTest=new Grille()
+GrilleTest.placerPositionBateau(0,0,0);
+GrilleTest.placerPositionBateau(0,0,1);
+GrilleTest.placerPositionBateau(1,2,0);
+GrilleTest.placerPositionBateau(1,2,1);
+GrilleTest.placerPositionBateau(1,2,2);
+
+#Grille[[0,-1,1,-1],[0,-1,1,-1],[-1,-1,1,-1]]
+
+if (!(GrilleTest.getBateau(2,2)==1)):
+    print('error getBateau');
+
+GrilleTest.supprimerPosition(2,0)
+
+if (GrilleTest.estBateau(2,0)):
+     print('error supprimerPosition');
+
+if (!(GrilleTest.envue(1,2)==True)):
+    print('error envue');
+
+if (!(GrilleTest.estDansGrille(4,2)==False)):
+    print('error estDansGrille');
+
+if (!(GrilleTest.estBateau(0,1)==True)):
+    print('error estBateau');
+
+if (!(GrilleTest.verificationCoordonnee(2,1)==False)):
+    print('error verificationCoordonnee);
+
+if (!(GrilleTest.estValide(1,2,0)==True)):
+    print('error estValide');
+
+if (!(GrilleTest.estVide()==False):
+    print('error estVide');
+
+if (!(GrilleTest.tirer(0,0)=='touché')):
+    print('error tirer pour touché')
+
+
+if (!(GrilleTest.tirer(0,1)=='coulé')):
+    print('error tirer pour coulé')
+
+
+
+#EEEEENNNNDDDD
+print('Testes Terminés');
